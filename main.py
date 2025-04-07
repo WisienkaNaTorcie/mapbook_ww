94f40c2e3f2f4b073a038d9b92eba48608c4508a
WisienkaNaTorcie <wiszniewska316@gmail.com>
2025-04-07 09:14:01 +0200

    WIP (work in progress): users list, simple loop, prepared

---
+++ main.py
@@ -0,0 +1,8 @@
+users:list[dict] = [
+    {'name': 'Wika', 'location': 'Radzyń Podlaski', 'posts': 1, },
+    {'name': 'Daria', 'location': 'Łosice', 'posts': 500, },
+    {'name': 'Kuba', 'location': 'Płock', 'posts': 200, }
+]
+
+for user in users:
+    print(f'Twój znajomy: {user["name"]} z {user["location"]} opublikował {user["posts"]} postów')

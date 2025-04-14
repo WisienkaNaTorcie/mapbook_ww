ba735192c26b3a103d7050477fd96b6d57809d48
WisienkaNaTorcie <wiszniewska316@gmail.com>
2025-04-14 08:56:53 +0200

    wip: new menu option:remove users

--- utills/controller.py
+++ utills/controller.py
@@ -7,3 +7,9 @@ def add_user(users_data: list[dict]) -> None:
     tmp_location:str = input('Podaj miejscowość: ')
     tmp_posts:int = int(input('Podaj liczbę postów: '))
     users_data.append({'name': tmp_name, 'location': tmp_location, 'posts': tmp_posts})
+
+def remove_user(users_data: list[dict]) -> None:
+        user_name = input('Podaj imię użytkownika do usunięcia: ')
+        for user in users_data:
+            if user['name'] == user_name:
+                users_data.remove(user)
